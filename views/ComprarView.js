c050fed3c302b78ccce76d4789ba8a85a6785464
Giolvani de Matos <inavloig@gmail.com>
2018-11-09 20:45:27 -0200

    criacao de novas telas

---
+++ views/ComprarView.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class ComprarView extends Component {
+    render(){
+        return (
+            <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+                <Text>Comprar</Text>
+            </View>
+        )
+    }
+}
+
+export default ComprarView;
