64d996e63bf07688a4400b24131e9a14da5caeb8
Diogo Augusto <diogo.augustoo@gmail.com>
2018-11-09 21:01:51 -0200

    Adicionado Button com Routes

--- views/HomeView.js
+++ views/HomeView.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 class HomeView extends Component {
   render() {
+    const { navigation } = this.props;
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Home Screen</Text>
+        <Button title="Login" onPress={() => navigation.push('Login')} />
+        <Button
+          title="Comprar Viagem"
+          onPress={() => navigation.push('Comprar')}
+        />
+        <Button
+          title="Minhas Viagens"
+          onPress={() => navigation.push('Viagens')}
+        />
+        <Button
+          title="Minha Conta"
+          onPress={() => navigation.push('MinhaConta')}
+        />
       </View>
     );
   }
--- views/RootView.js
+++ views/RootView.js
@@ -1,9 +1,25 @@
 import { createStackNavigator } from 'react-navigation';
 import HomeView from './HomeView';
+import LoginView from './LoginView';
+import ComprarView from './ComprarView';
+import MinhaContaView from './MinhaContaView';
+import ViagensView from './ViagensView';
 
 const RootView = createStackNavigator({
   Home: {
     screen: HomeView,
+  },
+  Login: {
+    screen: LoginView,
+  },
+  Comprar: {
+    screen: ComprarView,
+  },
+  MinhaConta: {
+    screen: MinhaContaView,
+  },
+  Viagens: {
+    screen: ViagensView,
   },
 });
 
